426542c5a10074f849314d7b0796d04f4806b340
abhinav3256 <104626092+abhinav3256@users.noreply.github.com>
2022-09-02 10:23:12 +0530

    Update connection.go

--- connection.go
+++ connection.go
@@ -11,7 +11,7 @@ import (
 const (
 	// TODO fill this in directly or through environment variable
 	// Build a DSN e.g. postgres://username:password@url.com:5432/dbName
-	DB_DSN = "postgres://postgres:abhinav@localhost:5432/lms?sslmode=disable"
+	DB_DSN = "postgres://vgjvojuiaupsfk:3fae18ef1d97d7abc31f38e6a4135e3fe154fee347ac12f0633e2c139788f85b@ec2-52-73-155-171.compute-1.amazonaws.com:5432/ddaene760ljqmv"
 )
 
 func CreateDBConnection() {
